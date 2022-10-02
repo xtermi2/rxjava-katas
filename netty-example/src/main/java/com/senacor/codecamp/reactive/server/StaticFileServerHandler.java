336571359b87d9215c47b04db22e235e641a5ce2
Andreas Keefer <xtermi2@users.noreply.github.com>
2022-10-02 12:08:40 +0200

    try to build with JDK 17

--- netty-example/src/main/java/com/senacor/codecamp/reactive/server/StaticFileServerHandler.java
+++ netty-example/src/main/java/com/senacor/codecamp/reactive/server/StaticFileServerHandler.java
@@ -7,8 +7,8 @@ import io.netty.handler.codec.http.*;
 import io.netty.handler.ssl.SslHandler;
 import io.netty.handler.stream.ChunkedFile;
 import io.netty.util.CharsetUtil;
+import jakarta.activation.MimetypesFileTypeMap;
 
-import javax.activation.MimetypesFileTypeMap;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
